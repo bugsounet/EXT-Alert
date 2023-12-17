9f5354c985c8bbe47083857e4e3c22d9cd9fb1a6
bugsounet <github@bugsounet.fr>
2023-12-17 16:24:05 +0100

    prepare GA v6

--- EXT-Alert.js
+++ EXT-Alert.js
@@ -39,8 +39,8 @@ Module.register("EXT-Alert", {
 
   notificationReceived: function(noti, payload, sender) {
     switch(noti) {
-      case "GW_READY":
-        if (sender.name == "Gateway") {
+      case "GA_READY":
+        if (sender.name == "MMM-GoogleAssistant") {
           this.sendSocketNotification("INIT", this.config)
           this.sendNotification("EXT_HELLO", this.name)
         }
