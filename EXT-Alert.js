2ba3d004a8466ed863446336d1e426c4bf44fe5f
bugsounet <github@bugsounet.fr>
2024-02-21 20:17:28 +0100

    lintify ;)

--- EXT-Alert.js
+++ EXT-Alert.js
@@ -1,11 +1,11 @@
 /**
  ** Module : EXT-Alert
  ** @bugsounet
- ** ©03-2023
+ ** ©02-2024
  ** support: https://forum.bugsounet.fr
  **/
 
-logALERT = (...args) => { /* do nothing */ }
+var logALERT = (...args) => { /* do nothing */ };
 
 Module.register("EXT-Alert", {
   requiresVersion: "2.25.0",
@@ -15,39 +15,51 @@ Module.register("EXT-Alert", {
     ignore: []
   },
 
-  start: function () {
-    new AlertConfig(this)
+  start () {
+    if (this.config.debug) logALERT = (...args) => { console.log("[ALERT]", ...args); };
+    this.AlertDisplay = new AlertDisplay();
+    this.AlertDisplay.prepareAlertPopup(); // better place for create main popup quickly !
+    const Tools = {
+      translate: (...args) => this.translate(...args)
+    };
+    this.AlertCommander = new AlertCommander(Tools);
+
+    //check ignore modules array (prevent crash)
+    if (!Array.isArray(this.config.ignore)) {
+      let tmp = this.config.ignore;
+      this.config.ignore = [];
+      if (tmp) this.config.ignore.push(tmp);
+    }
   },
 
-  getScripts: function() {
+  getScripts () {
     return [
-      "/modules/EXT-Alert/components/AlertInit.js",
       "/modules/EXT-Alert/components/AlertCommander.js",
-      "/modules/EXT-Alert/components/AlertDisplay.js",
-    ]
+      "/modules/EXT-Alert/components/AlertDisplay.js"
+    ];
   },
 
-  getStyles: function () {
-    return [ "EXT-Alert.css" ]
+  getStyles () {
+    return [ "EXT-Alert.css" ];
   },
 
-  getDom: function() {
-    var dom = document.createElement("div")
-    dom.style.display = 'none'
-    return dom
+  getDom () {
+    var dom = document.createElement("div");
+    dom.style.display = "none";
+    return dom;
   },
 
-  notificationReceived: function(noti, payload, sender) {
+  notificationReceived (noti, payload, sender) {
     switch(noti) {
       case "GA_READY":
-        if (sender.name == "MMM-GoogleAssistant") {
-          this.sendSocketNotification("INIT", this.config)
-          this.sendNotification("EXT_HELLO", this.name)
+        if (sender.name === "MMM-GoogleAssistant") {
+          this.sendSocketNotification("INIT", this.config);
+          this.sendNotification("EXT_HELLO", this.name);
         }
-        break
+        break;
       case "EXT_ALERT": // can be used all time (for GW starting error)
-        if (this.config.ignore.indexOf(sender.name) >= 0) return
-        if (!payload) return  this.AlertCommander.Alert("error", {message: "Alert error by:" + sender } )
+        if (this.config.ignore.indexOf(sender.name) >= 0) return;
+        if (!payload) return this.AlertCommander.Alert("error", { message: `Alert error by:${  sender}` } );
         this.AlertCommander.Alert({
           type: payload.type ? payload.type : "error",
           message: payload.message ? payload.message : "Unknow message",
@@ -55,8 +67,8 @@ Module.register("EXT-Alert", {
           sender: payload.sender ? payload.sender : sender.name,
           icon: payload.icon ? payload.icon: null,
           sound: payload.sound ? payload.sound: null
-        })
-        break
+        });
+        break;
     }
   }
-})
+});
--- components/AlertDisplay.js
+++ components/AlertDisplay.js
@@ -1,48 +1,48 @@
 class AlertDisplay {
   constructor () {
-    console.log("[ALERT] AlertDisplay Ready")
+    console.log("[ALERT] AlertDisplay Ready");
   }
 
   /***************************/
   /** Information Displayer **/
   /***************************/
 
-  prepareAlertPopup() {
-    var Alert = document.createElement("div")
-    Alert.id = "EXT-Alert"
-    Alert.style.zoom = "80%"
-    Alert.className= "hidden"
+  prepareAlertPopup () {
+    var Alert = document.createElement("div");
+    Alert.id = "EXT-Alert";
+    Alert.style.zoom = "80%";
+    Alert.className= "hidden";
 
-    var Alert_Bar = document.createElement("div")
-    Alert_Bar.id = "EXT-Alert-bar"
-    Alert_Bar.tabindex = -1
-    Alert.appendChild(Alert_Bar)
+    var Alert_Bar = document.createElement("div");
+    Alert_Bar.id = "EXT-Alert-bar";
+    Alert_Bar.tabindex = -1;
+    Alert.appendChild(Alert_Bar);
 
     //informations image
-    var Alert_Icon = document.createElement("img")
-    Alert_Icon.id= "EXT-Alert-Icon"
-    Alert_Icon.className="EXT-Alert-icon"
-    Alert_Icon.src = "/modules/EXT-Alert/resources/warning.gif"
-    Alert_Bar.appendChild(Alert_Icon)
+    var Alert_Icon = document.createElement("img");
+    Alert_Icon.id= "EXT-Alert-Icon";
+    Alert_Icon.className="EXT-Alert-icon";
+    Alert_Icon.src = "/modules/EXT-Alert/resources/warning.gif";
+    Alert_Bar.appendChild(Alert_Icon);
 
-    var Alert_Displayer = document.createElement("div")
-    Alert_Displayer.id= "EXT-Alert-Displayer"
-    Alert_Displayer.className="EXT-Alert-displayer"
-    Alert_Bar.appendChild(Alert_Displayer)
+    var Alert_Displayer = document.createElement("div");
+    Alert_Displayer.id= "EXT-Alert-Displayer";
+    Alert_Displayer.className="EXT-Alert-displayer";
+    Alert_Bar.appendChild(Alert_Displayer);
 
     //transcription informations text
-    var Alert_Response = document.createElement("span")
-    Alert_Response.id= "EXT-Alert-Message"
-    Alert_Response.className="EXT-Alert-response"
-    Alert_Response.textContent= "~EXT-Alert Displayer~"
-    Alert_Displayer.appendChild(Alert_Response)
+    var Alert_Response = document.createElement("span");
+    Alert_Response.id= "EXT-Alert-Message";
+    Alert_Response.className="EXT-Alert-response";
+    Alert_Response.textContent= "~EXT-Alert Displayer~";
+    Alert_Displayer.appendChild(Alert_Response);
 
-    var Alert_Sender = document.createElement("span")
-    Alert_Sender.id= "EXT-Alert-Sender"
-    Alert_Sender.className="EXT-Alert-sender"
-    Alert_Sender.textContent= "by EXT-Alert"
-    Alert_Displayer.appendChild(Alert_Sender)
+    var Alert_Sender = document.createElement("span");
+    Alert_Sender.id= "EXT-Alert-Sender";
+    Alert_Sender.className="EXT-Alert-sender";
+    Alert_Sender.textContent= "by EXT-Alert";
+    Alert_Displayer.appendChild(Alert_Sender);
 
-    document.body.appendChild(Alert)
+    document.body.appendChild(Alert);
   }
 }
