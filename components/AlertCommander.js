b3771ea01e10ca7b46f648c0771fcd0d0c3ada2f
bugsounet <github@bugsounet.fr>
2024-03-14 02:49:20 +0100

    begin coding with sweetalert2

--- components/AlertCommander.js
+++ components/AlertCommander.js
@@ -1,5 +1,6 @@
 class AlertCommander {
-  constructor (Tools) {
+  constructor (style,Tools) {
+    this.style = style
     this.alerts= {
       displayed: false,
       buffer: []
@@ -39,6 +40,7 @@ class AlertCommander {
     this.types.forEach((type) => {
       if (type.event === info.type) alertObject.type = type;
     });
+
     if (!info.message) { // should not happen
       logALERT("debug information:", info);
       return this.Alert("warning", { message: "Alert Core: No message!" });
@@ -56,19 +58,24 @@ class AlertCommander {
   /** Informations Display with translate from buffer **/
   AlertBuffer (type, message) {
     if (this.alerts.displayed || !this.alerts.buffer.length) return;
-    this.AlertLogo(type, message);
-    this.AlertInformations(message);
-    this.AlertShow();
-
     let timer = message.timer ? message.timer : type.timer;
 
     // define timer limit...
     if (timer < 3000) timer = 3000;
     if (timer > 30000) timer = 30000;
-
-    this.warningTimeout = setTimeout(() => {
-      this.AlertHide();
-    }, timer);
+    switch (this.style) {
+      case 0:
+        this.AlertLogo(type, message);
+        this.AlertInformations(message);
+        this.AlertShow();
+        this.warningTimeout = setTimeout(() => {
+          this.AlertHide();
+        }, timer);
+        break;
+      case 1:
+        this.SweetAlert(type,message,timer)
+        break;
+    }
   }
 
   AlertInformations (message) {
@@ -111,5 +118,48 @@ class AlertCommander {
     logALERT("Buffer deleted", this.alerts);
     if(this.alerts.buffer.length) this.AlertBuffer(this.alerts.buffer[0].type, this.alerts.buffer[0].info);
     else logALERT("Buffer is now empty!");
+  }
+
+  SweetAlert(type,message,timer) {
+    Swal.fire({
+      title: message.type, // to display... or not !?
+      html: message.message,
+      footer: message.sender ? message.sender : "EXT-Alert",
+      icon: type.event === "information" ? "info": type.event,
+      imageUrl: message.icon || undefined,
+      imageWidth: 100,
+      timer: timer,
+      showConfirmButton: false,
+      timerProgressBar: true,
+      background: "rgba(33,33,33,.99)",
+      color:"#ffffff",
+      showClass: {
+        popup: `
+          animate__animated
+          animate__zoomInDown
+          animate__faster
+        `
+      },
+      hideClass: {
+        popup: `
+          animate__animated
+          animate__zoomOutUp
+          animate__faster
+        `
+      },
+      customClass: {
+        timerProgressBar: "AlertProgressColor",
+        footer: "AlertFooterColor"
+      },
+      //toast: type.event === "error" ? false : true,
+      //width: "50em",
+      //position: "top"
+    })
+    this.alerts.displayed=true;
+    this.warningTimeout = setTimeout(() => {
+      this.AlertShift();
+    },timer);
+    if (message.sound === "none") return;
+    if (type.sound || message.sound) this.sound.src = `${message.sound ? message.sound : type.sound  }?seed=${Date.now}`;
   }
 }
